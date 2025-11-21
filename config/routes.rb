6a7f34f30e40a0693d548a8dbe339bf52a98391f
Felix Li <future.doo@gmail.com>
2025-11-21 18:59:18 +0000

    Add ActiveAdmin setup with AdminUser model and Devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
